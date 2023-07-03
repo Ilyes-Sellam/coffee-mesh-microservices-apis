afbbb1c4cc5408ef5577cc0b160f4c4fc88535c0
Ilyes-Sellam <ilyessellam04@gmail.com>
2023-07-03 14:49:51 +0100

    Implementation of URL query parameters for GET /orders

--- ch06/orders/orders/api/api.py
+++ ch06/orders/orders/api/api.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 from uuid import UUID, uuid4
 from http import HTTPStatus
 
@@ -16,8 +17,21 @@ ORDERS = []
 
 
 @app.get('/orders', response_model=GetOrdersSchema)
-def get_orders():
-    return {'orders': ORDERS}
+def get_orders(cancelled: Optional[bool] = None, limit: Optional[int] = None):
+    if cancelled is None and limit is None:
+        return {'orders': ORDERS}
+    else:
+        query_set = [order for order in ORDERS]
+        if cancelled != None:
+            if cancelled:
+                query_set = [
+                    order for order in query_set if order['status'] == 'cancelled']
+            else:
+                query_set = [
+                    order for order in query_set if order['status'] != 'cancelled']
+        if limit is not None and len(query_set) > limit:
+            return {'orders': query_set[:limit]}
+        return {'orders': query_set}
 
 
 @app.post('/orders',
